fdfe788f62570fc458c6e21d1dd7e54138f947c9
Alain <hello@alainbarthelemy.com>
2015-07-24 12:53:48 +0200

    ok

---
+++ src/about-template.php
@@ -0,0 +1,37 @@
+<?php /* Template Name: About Page Template */ get_header(); ?>
+
+	<main role="main">
+	
+		<section class="about">
+			<section class="cartouche">
+				<div class="first-block">
+					<div> Hémisphère </div>
+					<div> Atelier de dispositifs numériques </div>
+					<div> 234 avenue Felix Faure 69003 Lyon </div>
+				</div>
+				<div class="second-block">
+					<div>&lt;m.&gt; bonjour@hemisphere-project.com</div>
+					<div>&lt;t.&gt; [0033] 682 984 800</div>
+				</div>
+			
+			</section>
+			<section class="about-content">
+				<div class="images-column">
+				<?php echo types_render_field( "about_images", array("width" => "1200", "height" => "1200", "proportional" => "true" ) ) ;?>
+				</div>
+				<div class="text-column">
+					<div class="about-title">presentation :</div>
+					<div class="presentation-text"><?php echo types_render_field( "presentation", array( ) ); ?></div>
+					<div class="about-title">acteurs :</div>
+					<div class="acteurs-text"><?php echo types_render_field( "acteurs", array( ) ); ?></div>
+					<div class="about-title">newsletter :</div>
+					<div class="newsletter-text"></div>
+					<div class="about-title">credits :</div>
+					<div class="credits-text"><?php echo types_render_field( "credits", array( ) ); ?></div>
+				
+				</div>
+			</section>
+		</section>
+	</main>
+
+
--- src/header.php
+++ src/header.php
@@ -61,7 +61,7 @@
 						<div class="element" ><a href="<?php echo get_permalink( get_page_by_path( 'repertoire' ) );?>">Répertoire__</a></div>
 						<div class="element" ><a  href="<?php echo get_permalink( get_page_by_path( 'hemisphere' ) );?>">Hémisphère__</a></div>
 						<div class="element" ><a href="#">Laboratoire__</a></div>
-						<div class="element" ><a href="#">À propos__</a></div>
+						<div class="element" ><a href="<?php echo get_permalink( get_page_by_path( 'a-propos' ) );?>">À propos__</a></div>
 						<div class="element" ><a href="<?php echo get_search_link(''); ?>">Rechercher</a></div>
 
 					</div>
