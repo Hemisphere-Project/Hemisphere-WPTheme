2ec469781641f6243bcdd04ee0dcfb03021fcb6f
Salvadorpsartek <jeremie.forge@gmail.com>
2016-10-14 12:29:08 +0200

    responsive v1 ok

--- src/js/script-allpages.js
+++ src/js/script-allpages.js
@@ -11,6 +11,58 @@
 		// });
 
 
+		// var deletedPagesinMobile=["Projets", "Laboratoires"];
+
+		//////////////////////////////////////////////////////////
+		///////////////////   RESPONSIVE   ///////////////////////
+		//////////////////////////////////////////////////////////
+		// INIT SIZES
+
+		// var windowWidth = $(window).width();
+		// var screenSize = 'desktop';
+		// var screenCheck = true;
+		// var desktop = false;
+		// var init  = true;
+		//
+		// checkSizes();
+		// $( window ).resize(function() {
+		// 	checkSizes();
+		// });
+		//
+		// function checkSizes(){
+		// 	windowWidth = $(window).width();
+		// 	if((windowWidth > 768)&&((desktop==false)||(init==true))){
+		// 		setDesktop();
+		// 		desktop=true;
+		// 		init=false;
+		// 	}else if ((windowWidth < 768)&&((desktop==true)||(init==true))){
+		// 		setSmartphone();
+		// 		desktop=false;
+		// 		init=false;
+		// 		redirectIfWrongPage();
+		// 	}
+		// }
+		// function setDesktop(){
+		// 	console.log('window is desktop');
+		// }
+		// function setSmartphone(){
+		// 	console.log('window is mobile');
+		// }
+		// function redirectIfWrongPage(){
+		// 	// REDIRECT IF WRONG PAGE
+		// 	var pageTitle = document.title;
+		// 	$.each(deletedPagesinMobile, function(index, deletedpage){
+		// 		if (pageTitle.search(deletedpage)==0){
+		// 			console.log("WRONG PAGE");
+		// 			// window.location.replace('...');
+		// 		}
+		// 	});
+		// }
+
+
+
+
+
 
 
 	});
